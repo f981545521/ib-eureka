6dd961bb179ad344018746f1f74b861251ed8ff3
youfang <981545521@qq.com>
2018-06-21 14:08:36 +0800

    add : init import
    add : init import

---
+++ src/main/java/cn/acyou/ibeureka/IbEurekaApplication.java
@@ -0,0 +1,12 @@
+package cn.acyou.ibeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbEurekaApplication.class, args);
+	}
+}
